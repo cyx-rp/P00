c8bcb1dce1a69bf86f57c13b699c642da134cdca
cyx-rp <22022868@myrp.edu.sg>
2023-05-25 12:10:29 +0800

    first push to P00 repository in GitHub

---
+++ P00/src/Calculator.java
@@ -0,0 +1,24 @@
+
+public class Calculator {
+	
+	public int add(int a, int b) {
+		System.out.println("a:" + a + "b:" + b);
+		return a + b;
+	}
+	
+	public int subtract(int a, int b) {
+		return a - b;
+	}
+	
+	public int multiple(int a, int b) {
+		return a * b;
+	}
+	
+	public int divide(int a, int b) {
+		if (b == 0) {
+			System.out.println("Denominator is 0");
+		}
+		return a / b;
+	}
+	
+}
---
+++ P00/src/HelloWorld.java
@@ -0,0 +1,28 @@
+/*
+ * I declare that this code was written by me. 
+ * I do not copy or allow others to copy my code. 
+ * I understand that copying code is considered as plagiarism.
+ * 
+ * Student Name: chinyuxuan
+ * Student ID: {22022868}
+ * Class: {W65C}
+ * Date/Time created: Thursday 25-05-2023 09:13
+ */
+
+/**
+ * @author chinyuxuan
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("My 'first' Java program");
+		
+	}
+
+}
